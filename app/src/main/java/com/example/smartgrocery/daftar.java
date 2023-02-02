7204d13c9c93ed4f0e9f0dbd8b7cbe786679a970
rayhand <mrayhandpras@gmail.com>
2023-02-02 12:36:01 +0700

    update login dan register firebase

--- app/src/main/java/com/example/smartgrocery/daftar.java
+++ app/src/main/java/com/example/smartgrocery/daftar.java
@@ -1,31 +1,39 @@
 package com.example.smartgrocery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class daftar extends AppCompatActivity {
 
     EditText nama, email, password;
-    Button register;
     TextView masuk;
+    String textNama, textEmail, textPass;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daftar);
 
+        mAuth = FirebaseAuth.getInstance();
         nama = findViewById(R.id.daftar_nama);
         email = findViewById(R.id.login_email);
         password = findViewById(R.id.login_password);
-        register = findViewById(R.id.login_button);
-        masuk = findViewById(R.id.daftar);
+        masuk = findViewById(R.id.masuk);
 
         masuk.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,14 +43,42 @@ public class daftar extends AppCompatActivity {
             }
         });
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), login.class);
-                Toast.makeText(daftar.this, "Pendaftaran berhasil!", Toast.LENGTH_SHORT).show();
-                startActivity(intent);
-            }
-        });
+    }
+
+    public void cekDaftar(View v) {
+        textNama = nama.getText().toString();
+        textEmail = email.getText().toString();
+        textPass = password.getText().toString();
+
+//        if (TextUtils.isEmpty(textNama)) {
+//            Toast.makeText(getApplicationContext(), "Masukkan nama", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//        if (TextUtils.isEmpty(textEmail)) {
+//            Toast.makeText(getApplicationContext(), "Masukkan email", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//        if (TextUtils.isEmpty(textPass)) {
+//            Toast.makeText(getApplicationContext(), "Masukkan Password", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+
+        mAuth.createUserWithEmailAndPassword(textEmail, textPass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(daftar.this, "Akun berhasil dibuat!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), login.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(daftar.this, "Gagal buat akun!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 }
--- app/src/main/java/com/example/smartgrocery/login.java
+++ app/src/main/java/com/example/smartgrocery/login.java
@@ -1,28 +1,50 @@
 package com.example.smartgrocery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class login extends AppCompatActivity {
 
     EditText email, password;
-    Button loginbut;
     TextView register;
+    String textEmail, textPass;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        mAuth = FirebaseAuth.getInstance();
+
         email = findViewById(R.id.login_email);
         password = findViewById(R.id.login_password);
-        loginbut = findViewById(R.id.login_button);
         register = findViewById(R.id.daftar);
 
         register.setOnClickListener(new View.OnClickListener() {
@@ -32,25 +54,26 @@ public class login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
-        loginbut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String inputEmail = email.getText().toString();
-                String inputPassword = password.getText().toString();
-                if(email.getText().toString().equals("admin") && password.getText().toString().equals("fuyuk")) {
-                    Intent intent = new Intent(getApplicationContext(), mainpage.class);
-                    startActivity(intent);
-                }
-                else if (inputEmail.isEmpty() || inputPassword.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Email atau Password tidak boleh kosong!", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "Email atau Password salah!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+    public void cekMasuk(View v) {
+        textEmail = email.getText().toString();
+        textPass = password.getText().toString();
 
+        mAuth.signInWithEmailAndPassword(textEmail, textPass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(login.this, "Login berhasil!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), mainpage.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(login.this, "Email atau password salah!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 }
